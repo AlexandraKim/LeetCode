35e33088dba1cc739c0a2bbbbf4aa797f738aa54
Alexandra Kim <alexandra.success.kim@gmail.com>
2021-12-20 09:52:20 +0500

    SearchInsertPosition

---
+++ LeetCode.UnitTests/Easy/BinarySearch/SearchInsertPositionTests.cs
@@ -0,0 +1,23 @@
+using FluentAssertions;
+using LeetCode.Easy.BinarySearch;
+using NUnit.Framework;
+
+namespace LeetCode.UnitTests.Easy.BinarySearch
+{
+    public class SearchInsertPositionTests
+    {
+        [TestCase(new[] {1, 3, 5, 6}, 5, 2)]
+        [TestCase(new[] {1, 3, 5, 6}, 2, 1)]
+        [TestCase(new[] {1, 3, 5, 6}, 7, 4)]
+        [TestCase(new[] {1}, 2, 1)]
+        [TestCase(new[] {1, 3, 5}, 1, 0)]
+        [TestCase(new[] {2, 3, 4, 7, 8, 9}, 11, 6)]
+        [TestCase(new[] {1, 3, 5, 6 }, 0, 0)]
+        public void Test(int[] nums, int target, int expectedResult)
+        {
+            SearchInsertPosition.SearchInsert(nums, target)
+                .Should()
+                .Be(expectedResult);
+        }
+    }
+}
---
+++ LeetCode/Easy/BinarySearch/SearchInsertPosition.cs
@@ -0,0 +1,41 @@
+namespace LeetCode.Easy.BinarySearch {
+	public static class SearchInsertPosition {
+		public static int SearchInsert(int[] nums, int target) {
+			if (nums.Length == 0) {
+				return -1;
+			}
+			int start = 0;
+			int end = nums.Length - 1;
+			int mid = GetMidIndex(start, end);
+			while (start != mid) {
+				if (nums[mid] > target) {
+					end = mid;
+					mid = GetMidIndex(start, end);
+				} else {
+					start = mid;
+					mid = GetMidIndex(start, end);
+				}
+			}
+			if (nums[mid] == target) {
+				return mid;
+			}
+			if (nums[end] == target) {
+				return end;
+			}
+
+			if (nums[end] < target) {
+				return end + 1;
+			}
+
+			if (nums[mid] < target) {
+				return mid + 1;
+			}
+			
+			return mid;
+		}
+
+		private static int GetMidIndex(int start, int end) {
+			return (start + end) / 2;
+		}
+	}
+}
