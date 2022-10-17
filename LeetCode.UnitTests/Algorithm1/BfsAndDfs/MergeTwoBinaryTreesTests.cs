7a23f88932e53aa00d40691dd244e0af6789cef1
Alexandra Kim <alexandra.success.kim@gmail.com>
2022-10-18 00:45:04 +0500

    617. Merge Two Binary Trees https://leetcode.com/problems/merge-two-binary-trees/

---
+++ LeetCode.UnitTests/Algorithm1/BfsAndDfs/MergeTwoBinaryTreesTests.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using LeetCode.Algorithm1.BfsAndDfs;
+using NUnit.Framework;
+
+namespace LeetCode.UnitTests.Algorithm1.BfsAndDfs {
+    [TestFixture]
+    public class MergeTwoBinaryTreesTests {
+        [Test]
+        public void Test() {
+            var root1Node3 = new TreeNode(3, new TreeNode(5));
+            var root1 = new TreeNode(1, root1Node3, new TreeNode(2));
+            
+            var root2Node1 = new TreeNode(1, null, new TreeNode(4));
+            var root2Node3 = new TreeNode(3, null, new TreeNode(7));
+            var root2 = new TreeNode(2, root2Node1, root2Node3);
+            
+            var expectedResultNode4 = new TreeNode(4, new TreeNode(5), new TreeNode(4));
+            var expectedResultNode5 = new TreeNode(5, null, new TreeNode(7));
+            var expectedResult = new TreeNode(3, expectedResultNode4, expectedResultNode5);
+            
+            MergeTwoBinaryTrees.MergeTrees(root1, root2).Should().Be(expectedResult);
+        }
+        
+        [Test]
+        public void Test2() {
+            TreeNode root1 = null;
+            
+            var root2 = new TreeNode(1);
+            
+            var expectedResult = new TreeNode(1);
+            
+            MergeTwoBinaryTrees.MergeTrees(root1, root2).Should().Be(expectedResult);
+        }
+        
+        // [Test]
+        // public void Test3() {
+        //     var root1Node3 = new TreeNode(3, new TreeNode(5));
+        //     var root1 = new TreeNode(1, root1Node3, new TreeNode(2));
+        //     
+        //     var root2Node1 = new TreeNode(1, null, new TreeNode(4));
+        //     var root2Node3 = new TreeNode(3, null, new TreeNode(7));
+        //     var root2 = new TreeNode(2, root2Node1, root2Node3);
+        //     
+        //     var expectedResultNode4 = new TreeNode(4, new TreeNode(5), new TreeNode(4));
+        //     var expectedResultNode5 = new TreeNode(5, null, new TreeNode(7));
+        //     var expectedResult = new TreeNode(3, expectedResultNode4, expectedResultNode5);
+        //     
+        //     MergeTwoBinaryTrees.MergeTrees(root1, root2).Should().Be(expectedResult);
+        // }
+    }
+}
---
+++ LeetCode/Algorithm1/BfsAndDfs/MergeTwoBinaryTrees.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Algorithm1.BfsAndDfs {
+    // 617. Merge Two Binary Trees https://leetcode.com/problems/merge-two-binary-trees/
+    public class MergeTwoBinaryTrees {
+        public static TreeNode MergeTrees(TreeNode root1, TreeNode root2) {
+            if (root1 is null && root2 is null) {
+                return null;
+            }
+            TreeNode result = new TreeNode();
+            Queue<(TreeNode treeNode1, TreeNode treeNode2, TreeNode resultTreeNode)> q = 
+                new Queue<(TreeNode treeNode1, TreeNode treeNode2, TreeNode resultTreeNode)>();
+            result.val += root1?.val ?? 0;
+            result.val += root2?.val ?? 0;
+            q.Enqueue((root1, root2, result));
+        
+            while (q.Any()) {
+                (TreeNode treeNode1, TreeNode treeNode2, TreeNode resultTreeNode) = q.Dequeue();
+                if (treeNode1?.left != null || treeNode2?.left != null) {
+                    resultTreeNode.left = new TreeNode();
+                    if (treeNode1 is {left: { }}) {
+                        resultTreeNode.left.val += treeNode1.left.val;
+                    }
+                    if (treeNode2 is {left: { }}) {
+                        resultTreeNode.left.val += treeNode2.left.val;
+                    }
+                    q.Enqueue((treeNode1?.left, treeNode2?.left, resultTreeNode.left));
+                }
+            
+                if (treeNode1?.right != null || treeNode2?.right != null) {
+                    resultTreeNode.right = new TreeNode();
+                    if (treeNode1 is {right: { }}) {
+                        resultTreeNode.right.val += treeNode1.right.val;
+                    }
+
+                    if (treeNode2 is {right: { }}) {
+                        resultTreeNode.right.val += treeNode2.right.val;
+                    }
+
+                    q.Enqueue((treeNode1?.right, treeNode2?.right, resultTreeNode.right));
+                }
+            }
+        
+            return result;
+        }
+    }
+    
+  public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}
