c142e7d39726a438995e3bfcac1952a47a71bcea
Alexandra Kim <alexandra.success.kim@gmail.com>
2023-09-08 13:31:06 +0500

    upgarade .net

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace LeetCode {
 	internal class Program {
-		private static void Main() {
+		// private static void Main() {
 			// todo: convert to tests
 			// Console.WriteLine(RomanToInt.Convert("III"));
 			// Console.WriteLine(RomanToInt.Run("IV"));
@@ -9,6 +9,6 @@
 			// Console.WriteLine(RomanToInt.Run("MCMXCIV"));
 
 			// Console.WriteLine(LongestCommonPrefix.Run(new []{"flower","flow","flight"}));
-		}
+		// }
 	}
 }
