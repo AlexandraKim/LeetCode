b864043342cc8cf436d519cae0e0f7751c608ddd
Alexandra Kim <alexandra.success.kim@gmail.com>
2024-01-07 22:28:41 +0900

    move out helping methods to ListNode

--- LeetCode/ListNode.cs
+++ LeetCode/ListNode.cs
@@ -11,4 +11,19 @@ public class ListNode
         this.val = val;
         this.next = next;
     }
+
+    public static ListNode CreateListNodes(params int[] nums)
+    {
+        return CreateRecursive(nums, 0);
+    }
+
+    private static ListNode CreateRecursive(int[] nums, int i)
+    {
+        if (i == nums.Length - 1)
+        {
+            return new ListNode(nums[i]);
+        }
+
+        return new ListNode(nums[i], CreateRecursive(nums, i + 1));
+    }
 }
--- LeetCode/Medium/SortList/SortListTests.cs
+++ LeetCode/Medium/SortList/SortListTests.cs
@@ -9,9 +9,9 @@ public class SortListTests
 {
     private static IEnumerable<TestCaseData> TestCasesForFindingMid()
     {
-        yield return new TestCaseData(CreateListNodes(1, 3, 5, 8), CreateListNodes(3, 5, 8))
+        yield return new TestCaseData(ListNode.CreateListNodes(1, 3, 5, 8), ListNode.CreateListNodes(3, 5, 8))
             .SetName("1, 3, 5, 8 -> 3, 5, 8");
-        yield return new TestCaseData(CreateListNodes(1, 3, 5, 8, 9), CreateListNodes(5, 8, 9))
+        yield return new TestCaseData(ListNode.CreateListNodes(1, 3, 5, 8, 9), ListNode.CreateListNodes(5, 8, 9))
             .SetName("1, 3, 5, 8, 9 -> 5, 8, 9");
     }
 
@@ -27,9 +27,9 @@ public class SortListTests
     
     private static IEnumerable<TestCaseData> TestCasesForSplittingIntoTwo()
     {
-        yield return new TestCaseData(CreateListNodes(3, 5, 8), CreateListNodes(3), CreateListNodes(5, 8))
+        yield return new TestCaseData(ListNode.CreateListNodes(3, 5, 8), ListNode.CreateListNodes(3), ListNode.CreateListNodes(5, 8))
             .SetName("1, 3, 5, 8 -> 5, 8");
-        yield return new TestCaseData(CreateListNodes(5, 8, 9), CreateListNodes(5), CreateListNodes(8, 9))
+        yield return new TestCaseData(ListNode.CreateListNodes(5, 8, 9), ListNode.CreateListNodes(5), ListNode.CreateListNodes(8, 9))
             .SetName("1, 3, 5, 8, 9 -> 8, 9");
     }
     [TestCaseSource(nameof(TestCasesForSplittingIntoTwo))]
@@ -48,9 +48,9 @@ public class SortListTests
     
     private static IEnumerable<TestCaseData> TestCasesForMerge()
     {
-        yield return new TestCaseData(CreateListNodes(1, 3, 5, 8, 9, 10, 12), CreateListNodes(2, 4, 6, 7), CreateListNodes(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12))
+        yield return new TestCaseData(ListNode.CreateListNodes(1, 3, 5, 8, 9, 10, 12), ListNode.CreateListNodes(2, 4, 6, 7), ListNode.CreateListNodes(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12))
             .SetName("1, 3, 5, 8 + 2, 4, 6, 7");
-        yield return new TestCaseData(CreateListNodes(1, 3, 5, 8), CreateListNodes(2, 4, 6, 7), CreateListNodes(1, 2, 3, 4, 5, 6, 7, 8))
+        yield return new TestCaseData(ListNode.CreateListNodes(1, 3, 5, 8), ListNode.CreateListNodes(2, 4, 6, 7), ListNode.CreateListNodes(1, 2, 3, 4, 5, 6, 7, 8))
             .SetName("1, 3, 5, 8 + 2, 4, 6, 7");
     }
 
@@ -66,9 +66,9 @@ public class SortListTests
     
     private static IEnumerable<TestCaseData> TestCasesForSort()
     {
-        yield return new TestCaseData(CreateListNodes(2, 7, 1, 4, 3, 5, 8, 6), CreateListNodes(1, 2, 3, 4, 5, 6, 7, 8))
+        yield return new TestCaseData(ListNode.CreateListNodes(2, 7, 1, 4, 3, 5, 8, 6), ListNode.CreateListNodes(1, 2, 3, 4, 5, 6, 7, 8))
             .SetName("2, 7, 1, 4, 3, 5, 8, 6 -> 1, 2, 3, 4, 5, 6, 7, 8");
-        yield return new TestCaseData(CreateListNodes(4,2,1,3), CreateListNodes(1, 2, 3, 4))
+        yield return new TestCaseData(ListNode.CreateListNodes(4,2,1,3), ListNode.CreateListNodes(1, 2, 3, 4))
             .SetName("2, 7, 1, 4, 3, 5, 8, 6 -> 1, 2, 3, 4, 5, 6, 7, 8");
     }
 
@@ -80,20 +80,5 @@ public class SortListTests
         sut.Sort(head)
             .Should()
             .BeEquivalentTo(expectedResult);
-    }
-
-    private static ListNode CreateListNodes(params int[] nums)
-    {
-        return CreateRecursive(nums, 0);
-    }
-
-    private static ListNode CreateRecursive(int[] nums, int i)
-    {
-        if (i == nums.Length - 1)
-        {
-            return new ListNode(nums[i]);
-        }
-
-        return new ListNode(nums[i], CreateRecursive(nums, i + 1));
     }
 }
